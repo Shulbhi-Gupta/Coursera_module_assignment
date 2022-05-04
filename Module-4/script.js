dd8890bd8ebea8e275302e45b5d05ba1d2d5c470
Shulbhi-Gupta <Shulbhi.Gupta@ltts.com>
2022-05-04 21:02:50 +0530

    module-4 assign

---
+++ Module-4/script.js
@@ -0,0 +1,27 @@
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  } else {
+    helloSpeaker(names[i]);
+  }
+}
+
+function byeSpeaker(name) {
+  console.log("Hello" + " " + name);
+}
+
+
+function helloSpeaker(name) {
+  console.log("Good Bye" + " " + name);
+}
+
+
+  
+
+
+
